973d6aa49e4929fdebc0d87e7fcf5a8755ba0e82
Victor Costan <costan@gmail.com>
2011-04-26 02:46:44 -0400

    Added default layout and pwnfx.js to the mix.

--- lib/pwnstyles_rails/generators/install_generator.rb
+++ lib/pwnstyles_rails/generators/install_generator.rb
@@ -6,8 +6,18 @@ module PwnstylesRails
 
 # rails g pwnstyles_rails:install
 class InstallGenerator < UpdateGenerator
-  def stylesheets
-    copy_stylesheets []
+  def staic_assets
+    copy_static_assets []
+  end
+  
+  def layout
+    dir = File.expand_path 'layouts', File.dirname(__FILE__)
+    Dir.glob(File.join(dir, '*'), File::FNM_DOTMATCH).each do |source|
+      next if File.directory?(source)
+      source_file = source[(dir.length + 1)..-1]
+      dest = Rails.root.join 'app', 'views', 'layouts', source_file
+      copy_file source, dest
+    end
   end
 end  # class PwnstylesRails::AllGenerator
 
--- lib/pwnstyles_rails/generators/update_generator.rb
+++ lib/pwnstyles_rails/generators/update_generator.rb
@@ -6,21 +6,27 @@ module PwnstylesRails
 
 # rails g pwnstyles_rails:update
 class UpdateGenerator < Rails::Generators::Base
-  def stylesheets
-    copy_stylesheets ['scss/vars/_app.scss']
+  def static_assets
+    copy_static_assets ['scss/vars/_app.scss']
   end
 
 
   private
   
-  def copy_stylesheets(exclude_list = [])
-    dir = File.expand_path '../../../public/stylesheets', File.dirname(__FILE__)
+  def copy_static_assets(exclude_list = [])
+    copy_dir 'public/stylesheets', 'public/pwnstyles/stylesheets'
+    copy_dir 'public/javascripts', 'public/javascripts'
+  end
+  
+  def copy_dir(source_dir, destination_dir, exclude_list = [])
+    dir = File.expand_path File.join('../../..', source_dir),
+                           File.dirname(__FILE__)
     Dir.glob(File.join(dir, '**', '*'), File::FNM_DOTMATCH).each do |source|
       next if File.directory?(source)
       source_file = source[(dir.length + 1)..-1]
       next if exclude_list.include?(source_file)
       
-      dest = Rails.root.join 'public', 'pwnstyles', 'stylesheets', source_file
+      dest = Rails.root.join destination_dir, source_file
       copy_file source, dest
     end
   end
