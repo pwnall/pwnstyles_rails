0b27a0952605a3ef26ba4ccd62bc3ebef35786fa
Victor Costan <costan@gmail.com>
2011-04-28 06:06:22 -0400

    Scaffold for app-specific scss.

--- config/initializers/paths.rb
+++ config/initializers/paths.rb
@@ -2,6 +2,9 @@ require 'sass/plugin/rack'
 Sass::Plugin.add_template_location(
     Rails.root.join('public', 'pwnstyles', 'stylesheets', 'scss').to_s,
     Rails.root.join('public', 'pwnstyles', 'stylesheets').to_s)
+Sass::Plugin.add_template_location(
+    Rails.root.join('public', 'stylesheets', 'scss').to_s,
+    Rails.root.join('public', 'stylesheets').to_s)
 
 Sass::Plugin.options[:style] = Rails.env.development? ? :expanded : :compressed
 Sass::Plugin.options[:debug] = Rails.env.development?
--- lib/pwnstyles_rails/generators/update_generator.rb
+++ lib/pwnstyles_rails/generators/update_generator.rb
@@ -7,26 +7,24 @@ module PwnstylesRails
 # rails g pwnstyles_rails:update
 class UpdateGenerator < Rails::Generators::Base
   def static_assets
-    copy_static_assets ['scss/vars/_app.scss']
+    excludes = [
+      'pwnstyles/stylesheets/scss/vars/_app.scss',
+      'stylesheets/scss/application.scss'
+    ]
+    copy_static_assets excludes
   end
-
 
   private
   
   def copy_static_assets(exclude_list = [])
-    copy_dir 'public/stylesheets', 'public/pwnstyles/stylesheets', exclude_list
-    copy_dir 'public/javascripts', 'public/javascripts', exclude_list
-  end
-  
-  def copy_dir(source_dir, destination_dir, exclude_list = [])
-    dir = File.expand_path File.join('../../..', source_dir),
+    dir = File.expand_path File.join('../../../public'),
                            File.dirname(__FILE__)
     Dir.glob(File.join(dir, '**', '*'), File::FNM_DOTMATCH).each do |source|
       next if File.directory?(source)
       source_file = source[(dir.length + 1)..-1]
       next if exclude_list.include?(source_file)
       
-      dest = Rails.root.join destination_dir, source_file
+      dest = Rails.root.join 'public', source_file
       copy_file source, dest
     end
   end
