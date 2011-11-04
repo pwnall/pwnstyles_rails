6b1a3f31739be2cf67e75bfb2698b4d9c9c7be99
Victor Costan <costan@gmail.com>
2011-11-04 01:08:41 -0400

    Better engine structure and striped lists.

--- lib/pwnstyles_rails/engine.rb
+++ lib/pwnstyles_rails/engine.rb
@@ -6,13 +6,10 @@ require 'sass-rails'
 module PwnstylesRails
 
 class Engine < Rails::Engine
+  engine_name 'pwnstyles'
+  
   generators do
     require 'pwnstyles_rails/generators/all_generator.rb'
-  end
-  
-  initializer :pwnstyles_paths, :after => :setup_sass do |app|
-    app.config.sass.load_paths <<
-        File.expand_path('../../stylesheets', File.dirname(__FILE__))
   end
 end  # class PwnstylesRails::Engine
 
